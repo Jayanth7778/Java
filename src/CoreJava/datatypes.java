bbf401076b230f85e64f04bdd031c994bc473897
Jayanth7778 <gundalajayanthkumargoud@gmail.com>
2018-01-17 18:10:12 +0530

    Java Programs

---
+++ src/CoreJava/datatypes.java
@@ -0,0 +1,28 @@
+package CoreJava;
+
+public class datatypes 
+{
+	public static void main(String[] args)
+	{
+		byte myByte=46;
+		short myShort=454;
+		long myLong=45;
+		int myNum=53;
+		float myFloat=45.64f;
+		double myDouble=4500.55;
+		char myChar='$';
+		String myString="kjfr22";
+		boolean myBoolean=true;
+		
+		System.out.println(myByte);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myNum);
+		System.out.println(myFloat);
+		System.out.println(myDouble);
+		System.out.println(myChar);
+		System.out.println(myString);
+		System.out.println(myBoolean);
+	}
+
+}
---
+++ src/CoreJava/helloworld.java
@@ -0,0 +1,9 @@
+package CoreJava;
+
+public class helloworld 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello world..! Welcome to Java");
+	}
+}
